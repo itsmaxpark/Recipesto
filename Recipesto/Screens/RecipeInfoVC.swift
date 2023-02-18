13641acf895b7925a63abcbbfc2fc742d7be132a
Max Park <itsmaxpark@gmail.com>
2023-02-18 18:01:24 -0500

    removed scrollview from tableview

--- Recipesto/Screens/RecipeInfoVC.swift
+++ Recipesto/Screens/RecipeInfoVC.swift
@@ -22,8 +22,13 @@ class RecipeInfoVC: UIViewController {
     
     let titleLabel = RPTitleLabel(textAlignment: .center, fontSize: 32)
     let videoPlayer = AVPlayerViewController()
-    var tableView: UITableView = UITableView()
+    var tableView: UITableView = UITableView(frame: .zero, style: .grouped)
     var dataSource: UITableViewDiffableDataSource<Section, Component>?
+    
+    let scrollView = UIScrollView()
+    var headerView = UIView(frame: .zero)
+    
+    var width: CGFloat = ScreenSize.width
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +38,23 @@ class RecipeInfoVC: UIViewController {
         configureTableView()
         configureDataSource()
         reloadData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        if let headerView = tableView.tableHeaderView {
+
+            let height = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+            var headerFrame = headerView.frame
+
+            //Comparison necessary to avoid infinite loop
+            if height != headerFrame.size.height {
+                headerFrame.size.height = height
+                headerView.frame = headerFrame
+                tableView.tableHeaderView = headerView
+            }
+        }
     }
     
     func set(recipe: Item) {
@@ -45,20 +67,6 @@ class RecipeInfoVC: UIViewController {
         titleLabel.text = name
     }
     
-    private func configureTableView() {
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.delegate = self
-        tableView.backgroundColor = .systemPink
-        tableView.register(IngredientCell.self, forCellReuseIdentifier: IngredientCell.identifier)
-    }
-    
-    private func configureDataSource() {
-        dataSource = UITableViewDiffableDataSource(tableView: tableView, cellProvider: { tableView, indexPath, component in
-            let cell = tableView.dequeueReusableCell(withIdentifier: IngredientCell.identifier, for: indexPath) as! IngredientCell
-            cell.set(component: component)
-            return cell
-        })
-    }
     
     func reloadData() {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Component>()
@@ -77,6 +85,21 @@ class RecipeInfoVC: UIViewController {
         videoPlayer.showsPlaybackControls = true
         videoPlayer.view.translatesAutoresizingMaskIntoConstraints = false
     }
+    private func configureTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.backgroundColor = .systemPink
+        tableView.register(IngredientCell.self, forCellReuseIdentifier: IngredientCell.identifier)
+        tableView.alwaysBounceVertical = false
+    }
+    
+    private func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource(tableView: tableView, cellProvider: { tableView, indexPath, component in
+            let cell = tableView.dequeueReusableCell(withIdentifier: IngredientCell.identifier, for: indexPath) as! IngredientCell
+            cell.set(component: component)
+            return cell
+        })
+    }
     
     private func configureVC() {
         view.backgroundColor = .systemBackground
@@ -85,29 +108,59 @@ class RecipeInfoVC: UIViewController {
     }
     
     private func configureUI() {
-        view.addSubviews(titleLabel, videoPlayer.view, tableView)
         let padding: CGFloat = 6
         let width: CGFloat = ScreenSize.width
+        view.addSubview(tableView)
+//        headerView.addSubviews(titleLabel, videoPlayer.view)
+//        tableView.tableHeaderView = headerView
+//        headerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
-            titleLabel.heightAnchor.constraint(equalToConstant: 80),
-            
-            videoPlayer.view.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: padding),
-            videoPlayer.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            videoPlayer.view.widthAnchor.constraint(equalToConstant: width),
-            videoPlayer.view.heightAnchor.constraint(equalToConstant: width),
-            
-            tableView.topAnchor.constraint(equalTo: videoPlayer.view.bottomAnchor, constant: padding),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.heightAnchor.constraint(equalToConstant: 200)
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+//            titleLabel.topAnchor.constraint(equalTo: headerView.topAnchor),
+//            titleLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+//            titleLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+//            titleLabel.heightAnchor.constraint(equalToConstant: 80),
+//
+//            videoPlayer.view.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+//            videoPlayer.view.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+//            videoPlayer.view.widthAnchor.constraint(equalToConstant: width),
+//            videoPlayer.view.heightAnchor.constraint(equalToConstant: width),
+
         ])
     }
 }
 
 extension RecipeInfoVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 80+width
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        
+        let headerView = UIView()
+        headerView.addSubviews(titleLabel, videoPlayer.view)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: headerView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 80),
+
+            videoPlayer.view.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            videoPlayer.view.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+            videoPlayer.view.widthAnchor.constraint(equalTo: headerView.widthAnchor),
+            videoPlayer.view.heightAnchor.constraint(equalTo: headerView.heightAnchor),
+        ])
+        return headerView
+    }
     
 }
